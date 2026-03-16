27a75f6747d2534fb62254e43c88f6321973d1df
max254 <maxkamau77@gmail.com>
2026-03-16 16:04:00 +0300

    added different facilities and chps

---
+++ backend/backend/src/seed.js
@@ -0,0 +1,97 @@
+import dotenv from 'dotenv'
+dotenv.config();
+
+import mongoose from 'mongoose';
+import Facility from './models/Facility.js';
+import Chp from './models/Chp.js';
+
+await mongoose.connect(process.env.MONGODB_URI);
+console.log('Connected');
+
+// clear existing data
+await Facility.deleteMany();
+await Chp.deleteMany();
+
+// Seed Facilities
+await Facility.insertMany([
+  {
+    name: "Thika Level 5 Hospital",
+    type: "hospital",
+    county: "Thika",
+    ward: "Township",
+    phone: "+25470000000001",
+  },
+  {
+    name: "Mary-Help Hospital",
+    type: "hospital",
+    county: "Kiambu",
+    ward: "Township",
+    phone: "+254700000002",
+  },
+  {
+    name: "Ruiru Level 4",
+    type: "hospital",
+    county: "Kiambu",
+    ward: "Ruiru",
+    phone: "+254700000003",
+  },
+  {
+    name: "Avenue Hospital",
+    type: "hospital",
+    county: "Kiambu",
+    ward: "Township",
+    phone: "+254700000004",
+  },
+  {
+    name: "Makongeni Health Centre",
+    type: "health_centre",
+    county: "Kiambu",
+    ward: "Makongeni",
+    phone: "+24570000000005",
+  },
+]);
+console.log('Facilities seeded');
+
+// seed Chps
+await Chp.insertMany([
+  {
+    name: "Peter Kamau",
+    phone: "+254712000001",
+    ward: "Makongeni",
+    county: "Kiambu",
+    isVerified: true,
+  },
+  {
+    name: "Grace Njeri",
+    phone: "+254712000002",
+    ward: "Ruiru",
+    county: "Kiambu",
+    isVerified: true,
+  },
+  {
+    name: "John Mwangi",
+    phone: "+254712000003",
+    ward: "Township",
+    county: "Nairobi",
+    isVerified: true,
+  },
+  {
+    name: "Fatuma Omar",
+    phone: "+254712000004",
+    ward: "Igegania",
+    county: "Kiambu",
+    isVerified: true,
+  },
+  {
+    name: "James Odhiambo",
+    phone: "+254712000005",
+    ward: "Landless",
+    county: "Kiambu",
+    isVerified: true,
+  },
+]);
+console.log('Chps seeded');
+
+await mongoose.disconnect();
+console.log('seed complete');
+process.exit(0);
