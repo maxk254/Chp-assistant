a561f07bbf49e5273570d022fce9ff4bdad5533d
max254 <maxkamau77@gmail.com>
2026-03-17 01:58:38 +0300

    get api to dispaly facilities detils

--- backend/backend/src/routes/facilities.js
+++ backend/backend/src/routes/facilities.js
@@ -1,6 +1,24 @@
 // get/api/facilities
-// src/routes/facilities.js
 import express from 'express';
+import Facility from '../models/Facility.js';
+
 const router = express.Router();
-router.get('/', (req, res) => res.json({ facilities: [] }));
+
+// Get /api/facilities
+router.get('/', async (req, res) => {
+  try {
+    const { county, type} = req.query;
+
+    const filter = {};
+    if (county) filter.county = county;
+    if (type) filter.type = type;
+
+    const facilities = await Facility.find(filter);
+
+    res.json({ success: true, facilities});
+
+  } catch (err) {
+    res.status(500).json({ success: false, error: err.message});
+  }
+});
 export default router;
